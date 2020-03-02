1818a7ae4f5e993b752c03e65876ffa9562e4928
Unear <andres_mau.gutierrez@uao.edu.co>
2020-03-02 13:35:21 -0500

    Version Maquetar proyecto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,17 +3,22 @@ import { Routes, RouterModule } from '@angular/router';
 import { ClientesComponent } from './clientes/clientes.component';
 import { ProductosComponent } from './productos/productos.component';
 import { PedidosComponent } from './pedidos/pedidos.component';
+import { HomeComponent } from './home/home.component';
+import { AgregarClientesComponent } from './agregar-clientes/agregar-clientes.component';
 
 
 const routes: Routes = [
   {
-    path: '', component: ClientesComponent // Path = '' significa que es la pagina que primero se cargará
+    path: '', component: HomeComponent // Path = '' significa que es la pagina que primero se cargará
   },
   {
     path: 'productos', component: ProductosComponent
   },
   {
     path: 'pedidos', component: PedidosComponent
+  },
+  {
+    path: 'agregar-Clientes', component: AgregarClientesComponent
   }
 ];
 
